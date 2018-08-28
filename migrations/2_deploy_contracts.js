8ac58efa936ace4fe020d8a2bffb8a043e27e21b
Carter Duncan <dukeblue2017@gmail.com>
2018-08-27 23:18:32 -0500

    Update page title, remove cruft

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var SubmitArticle = artifacts.require("./SubmitArticle.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(SimpleStorage);
   deployer.deploy(SubmitArticle);
 };
